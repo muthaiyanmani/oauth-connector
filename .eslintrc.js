fed71b99bb0c1b207f88fbbe196f4d6a11644c31
Muthaiyan M <muthaiyan.pro@gmail.com>
2025-11-27 21:15:30 +0530

    build: added tsup for bundling

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
+  ignorePatterns: ['tsup.config.ts', '*.config.js', '*.config.ts'],
 };
 
